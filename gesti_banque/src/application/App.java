500348b21eba2b00da5a22a6df895d62c91e74fb
gilert <crankyfox@protonmail.com>
2024-01-16 16:09:42 +0100

    fix: add LocaleDate import

--- gesti_banque/src/application/App.java
+++ gesti_banque/src/application/App.java
@@ -2,16 +2,20 @@ package application;
 
 import entities.Banque;
 import entities.CompteBancaire;
+import java.time.LocalDate;
 
 public class App {
 
 	public static void main(String[] args) {
 		Banque banque = new Banque(100, "GK_GestiBank", "100 Av. Albert 1er, 92500 Rueil-Malmaison, France",
 				1000_000_000);
-		CompteBancaire cb1 = new CompteBancaire(10101010,Mohamed Amine MEZGHICH,2000,LocalDate.of(2024, 1, 15), 100);
-		CompteBancaire cb2 = new CompteBancaire(20202020,Jean Paul,3000,LocalDate.of(2024, 1, 10), 100);
+		CompteBancaire cb1 = new CompteBancaire(
+				10101010, "Mohamed Amine MEZGHICH", 2000, LocalDate.of(2024, 1, 15), 100);
+		CompteBancaire cb2 = new CompteBancaire(
+				20202020, "Jean Paul", 3000, LocalDate.of(2024, 1, 10), 100);
 
-		System.out.println("banque");
+		System.out.println("cb1: " + cb1);
+		System.out.println("cb2: " + cb2);
 
 
 
